8001c6fdde5cc1eb6d7ccf5f486c2d2d5f00ba17
Mauricio Poppe <mauricio.poppe@gmail.com>
2016-04-19 00:00:15 -0400

    Redirect header for the docs

---
+++ redirect.js
@@ -0,0 +1,12 @@
+var fs = require('fs')
+var version = require('./package.json').version
+
+var output = fs.createWriteStream('./docs/interval-arithmetic/index.html')
+var url = 'http://maurizzzio.github.io/interval-arithmetic/' + version + '/'
+var str = '<head>'
+str += '<meta http-equiv="refresh" content="0; URL=\'' + url + '\'" />'
+str += '</head>'
+
+output.write(str)
+output.end()
+
