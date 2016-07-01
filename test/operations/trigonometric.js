b46ca929e0fdb2817c050a910b2a92f10096ee5a
Mauricio Poppe <mauricio.poppe@gmail.com>
2016-07-01 02:52:18 -0400

    lint

--- test/operations/trigonometric.js
+++ test/operations/trigonometric.js
@@ -64,8 +64,8 @@ describe('trigonometric', function () {
     Interval.almostEqual(n, [-1, 0])
     n = trigonometric.sin(new Interval(-2 * Math.PI, -3 * Math.PI / 2))
     Interval.almostEqual(n, [0, 1])
-    var p = 2 * Math.PI;
-    n = trigonometric.sin(new Interval(-5*p -2 * Math.PI, -5*p -3 * Math.PI / 2))
+    var p = 2 * Math.PI
+    n = trigonometric.sin(new Interval(-5 * p - 2 * Math.PI, -5 * p - 3 * Math.PI / 2))
     Interval.almostEqual(n, [0, 1])
   })
 
